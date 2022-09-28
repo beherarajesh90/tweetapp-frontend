bb60d549fa455e4d9aa3eb5a1d31171625fc11f7
beherarajesh90 <33206164+beherarajesh90@users.noreply.github.com>
2022-09-28 16:43:22 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// const ROOT_URL = "https://cors-everywhere.herokuapp.com/http://13.233.88.107:8080/api/v1.0/tweetapi";
-const ROOT_URL = "http://13.233.88.107:8080/api/v1.0/tweetapi";
+const ROOT_URL = "https://cors-anywhere.herokuapp.com/http://13.233.88.107:8080/api/v1.0/tweetapi";
+// const ROOT_URL = "http://13.233.88.107:8080/api/v1.0/tweetapi";
 const API_URLS = {
   login: () => `${ROOT_URL}/login`,
   register: () => `${ROOT_URL}/register`,

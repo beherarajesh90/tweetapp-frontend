fce23d3bfe088463ee497f9b55779a9f85654e2f
beherarajesh90 <33206164+beherarajesh90@users.noreply.github.com>
2022-09-28 13:39:08 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const ROOT_URL = "http://52.66.24.250:8080/api/v1.0/tweetapi";
+const ROOT_URL = "https://cors-everywhere.herokuapp.com/http://13.233.88.107:8080/api/v1.0/tweetapi";
 const API_URLS = {
   login: () => `${ROOT_URL}/login`,
   register: () => `${ROOT_URL}/register`,
